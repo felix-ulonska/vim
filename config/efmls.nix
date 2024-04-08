1bf061c4f7ddc77b2c30bf613c41860989302fca
Felix Ulonska <git@webfoo.de>
2024-04-08 17:03:29 +0200

    fix: prettier

--- config/efmls.nix
+++ config/efmls.nix
@@ -1,5 +1,5 @@
 {
 	plugins.efmls-configs.enable = true;
-	plugins.efmls-configs.setup.typescript.formatter = ["prettier_d"];
+	plugins.efmls-configs.setup.typescript.formatter = ["prettier"];
 	plugins.efmls-configs.setup.typescript.linter = [ "eslint" ];
 }
