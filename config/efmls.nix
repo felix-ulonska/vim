94deb9af7547b4b705de88da629edcff7c0fe58c
Felix Ulonska <git@webfoo.de>
2024-04-09 10:46:46 +0200

    feat: added ltex linter

--- config/efmls.nix
+++ config/efmls.nix
@@ -1,5 +1,5 @@
 {
 	plugins.efmls-configs.enable = true;
 	plugins.efmls-configs.setup.typescript.formatter = ["prettier"];
-	plugins.efmls-configs.setup.typescript.linter = [ "eslint" ];
+	plugins.efmls-configs.setup.typescript.linter = [ "eslint" "ltex" ];
 }
