d3a167ba10fece3cc7dffb1bdf8e7a0c80f9a4dd
Felix Ulonska <git@webfoo.de>
2025-06-15 16:21:03 +0200

    fixed keymapped

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -3,8 +3,8 @@
     enable = true;
     settings = {
       keymap_new = "<Leader>tn";
-      keymap_prev = "<Leader>tp";
-      keymap_next = "<Leader>tn";
+      keymap_prev = "<Leader>th";
+      keymap_next = "<Leader>tl";
       keymap_toggle = "<Leader>tt";
       keymap_kill = "<Leader>tk";
     };
